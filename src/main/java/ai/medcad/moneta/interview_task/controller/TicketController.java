16663e73106b9fed7cea374c5779a952d71d7006
paveln <pavel.nevyhosteny@medcad.ai>
2024-11-26 11:09:31 +0100

    Adding full implementation of REST service

--- src/main/java/ai/medcad/moneta/interview_task/controller/TicketController.java
+++ src/main/java/ai/medcad/moneta/interview_task/controller/TicketController.java
@@ -1,10 +1,37 @@
 package ai.medcad.moneta.interview_task.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ai.medcad.moneta.interview_task.model.TicketDTO;
+import ai.medcad.moneta.interview_task.service.TicketStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/ticket")
+@RequestMapping("/api/v1/ticket/")
 public class TicketController {
 
+    private final TicketStoreService ticketStoreService;
+
+    public TicketController(TicketStoreService ticketStoreService) {
+        this.ticketStoreService = ticketStoreService;
+    }
+
+    @GetMapping
+    public ResponseEntity<TicketDTO> getFirstTicket() {
+        TicketDTO actualTicket = ticketStoreService.getFirstTicketInQueue();
+        return new ResponseEntity<>(actualTicket, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity createTicket() {
+        TicketDTO newTicket = ticketStoreService.createTicket();
+        return new ResponseEntity(newTicket, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeFirstTicket() {
+        ticketStoreService.removeFirstInQueueTicket();
+    }
 }
